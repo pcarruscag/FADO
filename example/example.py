33d938e8afc57d62f95fd57319cb3bb501a3603b
Pedro Gomes <pcarruscag@gmail.com>
2019-11-24 19:09:34 +0000

    start putting together example to drive hard bits of the development

---
+++ example/example.py
@@ -0,0 +1,10 @@
+# This example will serve to define the "interface" of the framework.
+# It should mimic the way an external code works so that it can drive the development
+# and highlight problems with the design as early as possible.
+#
+# For generality:
+# 3 functions that result from 2 evaluation of the primal function
+#
+# Files:
+#  - direct.py is the primal solver, it takes a data file and a config.
+#    Two data files will 
