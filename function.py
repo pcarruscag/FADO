93fe7b0f6206adb597e364d9a578cff7d2a8b181
Pedro Gomes <pcarruscag@gmail.com>
2019-11-24 23:05:37 +0000

    function to quantify the non-discreteness of variables

--- function.py
+++ function.py
@@ -16,23 +16,14 @@
 #  along with FADO.  If not, see <https://www.gnu.org/licenses/>.
 
 import numpy as np
+import abc
 
-# Class to define functions
-class Function:
-    def __init__(self,name="",outFile="",outParser=None):
+# Abstract base class for functions
+class FunctionBase(abc.ABC):
+    def __init__(self,name):
         self._name = name
-
-        # where and how the output value is obtained
-        self.setOutput(outFile,outParser)
-        
-        # evaluation pipelines for value and gradient
-        self._funEval = []
-        self._gradEval = []
-
-        # define inputs, where and how their gradients are obtained
+        # inputs
         self._variables = []
-        self._gradFiles = []
-        self._gradParse = []
 
     def getName(self,maxLen=0):
         name = self._name
@@ -40,13 +31,59 @@ class Function:
         if maxLen<len(name): name = name[:maxLen]
         return name
 
+    def getVariables(self):
+        return self._variables
+
+    @abc.abstractmethod
+    def getParameters(self):
+        return NotImplemented
+
+    @abc.abstractmethod
+    def getValue(self):
+        return NotImplemented
+
+    @abc.abstractmethod
+    def getGradient(self,mask):
+        return NotImplemented
+
+    @abc.abstractmethod
+    def resetValueEvalChain(self):
+        return NotImplemented
+
+    @abc.abstractmethod
+    def resetGradientEvalChain(self):
+        return NotImplemented
+
+    @abc.abstractmethod
+    def getValueEvalChain(self):
+        return NotImplemented
+
+    @abc.abstractmethod
+    def getGradientEvalChain(self):
+        return NotImplemented
+#end
+
+
+# Class to define evaluation-based functions
+class Function(FunctionBase):
+    def __init__(self,name="",outFile="",outParser=None):
+        FunctionBase.__init__(self,name)
+
+        # where and how the output value is obtained
+        self.setOutput(outFile,outParser)
+
+        # evaluation pipelines for value and gradient
+        self._funEval = []
+        self._gradEval = []
+
+        # where and how their gradients are obtained
+        self._gradFiles = []
+        self._gradParse = []
+
     def addInputVariable(self,variable,gradFile,gradParser):
         self._variables.append(variable)
         self._gradFiles.append(gradFile)
         self._gradParse.append(gradParser)
-
-    def getVariables(self):
-        return self._variables
 
     def getParameters(self):
         parameters = []
@@ -134,3 +171,77 @@ class Function:
     def getGradientEvalChain(self):
         return self._gradEval
 #end
+
+
+# Continuous measure of non-dscreteness (usually to use as a constraint)
+class NonDiscreteness(FunctionBase):
+    def __init__(self,name=""):
+        FunctionBase.__init__(self,name)
+
+    def addInputVariable(self,variable):
+        self._variables.append(variable)
+
+    def getValue(self):
+        y = 0.0
+        N = 0
+        for var in self._variables:
+            N += var.getSize()
+            x  = var.getCurrent()
+            lb = var.getLowerBound()
+            ub = var.getUpperBound()
+            y += ((ub-x)*(x-lb)/(ub+lb)**2).sum()
+        return 4*y/N
+
+    def getGradient(self,mask=None):
+        # determine size of gradient vector
+        N = 0
+        for var in self._variables:
+            N += var.getSize()
+
+        size = 0
+        if mask is None:
+            size = N
+        else:
+            for var in mask.keys():
+                size += var.getSize()
+
+        # populate gradient vector
+        gradient = np.ndarray((size,))
+        idx = 0
+        for var in self._variables:
+            x  = var.getCurrent()
+            lb = var.getLowerBound()
+            ub = var.getUpperBound()
+            grad = (4.0/N)*(ub+lb-2*x)/(ub+lb)**2
+
+            if mask is not None: idx = mask[var]
+
+            try:
+                for val in grad:
+                    gradient[idx] = val
+                    idx += 1
+            except:
+                gradient[idx] = grad
+                idx += 1
+            #end
+        #end
+
+        return gradient
+    #end
+
+    def getParameters(self):
+        return []
+
+    def resetValueEvalChain(self):
+        pass
+
+    def resetGradientEvalChain(self):
+        pass
+
+    def getValueEvalChain(self):
+        return []
+
+    def getGradientEvalChain(self):
+        return []
+#end
+
