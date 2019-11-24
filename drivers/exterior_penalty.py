088bb8bfdf53cfd0ed0fa0af39bee19e45ba19ee
Pedro Gomes <pcarruscag@gmail.com>
2019-11-24 22:38:47 +0000

    store gradient as class member to avoid local variable, do the update at the end of "grad"

--- drivers/exterior_penalty.py
+++ drivers/exterior_penalty.py
@@ -46,6 +46,9 @@ class ExteriorPenaltyDriver(DriverBase):
         self._gtpen = None
         self._inpen = None
 
+        # gradient vector
+        self._grad = None
+
         # timers, counters, flags
         self._funEval = 0
         self._funTime = 0
@@ -69,6 +72,8 @@ class ExteriorPenaltyDriver(DriverBase):
         self._gtpen = np.ones((len(self._constraintsGT),))*self._rini
         self._inpen = np.ones((len(self._constraintsIN),))*self._rini
 
+        self._grad = np.zeros((self.getNumVariables(),))
+
         self._isInit = True
     #end
 
@@ -77,11 +82,6 @@ class ExteriorPenaltyDriver(DriverBase):
 
         # update the values of the variables
         self._setCurrent(x)
-
-        # update the values of the parameters
-        if self._jacEval % self._freq is 0 and self._jacEval is not 0:
-            for par in self._parameters:
-                par.increment()
 
         # evaluate everything (sequential for now)
         self._funEval += 1
@@ -137,35 +137,40 @@ class ExteriorPenaltyDriver(DriverBase):
     def grad(self,x):
         # initializing and updating values was done when evaluating the function
 
-        # evaluate all required gradients (skip those where constraint is not active)
+        # evaluate all required gradients (skip those where the constraint is not active)
         self._jacEval += 1
         self._jacTime -= time.time()
 
-        gradient = np.zeros((self.getNumVariables(),))
+        self._grad *= 0.0
 
         for obj in self._objectives:
-            gradient += obj.function.getGradient()*obj.scale
+            self._grad += obj.function.getGradient()*obj.scale
 
         for (obj,f,r) in zip(self._constraintsEQ,self._eqval,self._eqpen):
-            gradient += 2.0*r*f*obj.function.getGradient()*obj.scale
+            self._grad += 2.0*r*f*obj.function.getGradient()*obj.scale
 
         for (obj,f,r) in zip(self._constraintsLT,self._ltval,self._ltpen):
-            if f > 0.0: gradient += 2.0*r*f*obj.function.getGradient()*obj.scale
+            if f > 0.0: self._grad += 2.0*r*f*obj.function.getGradient()*obj.scale
 
         for (obj,f,r) in zip(self._constraintsGT,self._gtval,self._gtpen):
-            if f < 0.0: gradient += 2.0*r*f*obj.function.getGradient()*obj.scale
+            if f < 0.0: self._grad += 2.0*r*f*obj.function.getGradient()*obj.scale
 
         for (obj,f,r) in zip(self._constraintsIN,self._inval,self._inpen):
-            if f>1.0 or f<0.0: gradient += 2.0*r*f*obj.function.getGradient()*obj.scale
+            if f>1.0 or f<0.0: self._grad += 2.0*r*f*obj.function.getGradient()*obj.scale
+
+        self._grad /= self._varScales
 
         self._jacTime += time.time()
 
-        return gradient/self._varScales
+        # update penalties and params (evaluating the gradient concludes an outer iteration)
+        if self._jacEval % self._freq is 0: self._update()
+
+        return self._grad
     #end
 
     # if the constraint is active and above tolerance increase the penalty
     # otherwise decrease (minimum and maximum are constrained)
-    def update(self):
+    def _update(self):
         # equality (always active)
         for i in range(self._eqpen.size):
             if abs(self._eqval[i]) > self._tol:
@@ -191,6 +196,10 @@ class ExteriorPenaltyDriver(DriverBase):
                 self._inpen[i] = min(self._inpen[i]*self._cup,self._rmax)
             elif self._inval[i] < 1.0 and self._inval[i] > 0.0:
                 self._inpen[i] = max(self._inpen[i]*self._cdown,self._rini)
+
+        # update the values of the parameters
+        for par in self._parameters:
+            par.increment()
     #end
 
 #end
