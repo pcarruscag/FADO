8b506118d73af4ee70b82fc5b9ae7f2c15bb8242
Pedro Gomes <pcarruscag@gmail.com>
2021-03-29 18:17:48 +0100

    update testcases for SU2 v7.1.0

--- examples/example2_SU2/example.py
+++ examples/example2_SU2/example.py
@@ -8,8 +8,6 @@ subprocess.call("unzip -o ../example1_SU2/data.zip",shell=True)
 rho = InputVariable(0.5,TableWriter("  ",(1,-1)),1600,1.0,0.0,1.0)
 
 # Parameters
-pType_direct = Parameter(["DIRECT"],LabelReplacer("__MATH_PROBLEM__"))
-pType_adjoint = Parameter(["DISCRETE_ADJOINT"],LabelReplacer("__MATH_PROBLEM__"))
 fType_objective = Parameter(["REFERENCE_NODE"],LabelReplacer("__FUNCTION__"))
 fType_constraint = Parameter(["VOLUME_FRACTION"],LabelReplacer("__FUNCTION__"))
 beta = Parameter([0.01, 1, 4, 16, 64, 200],LabelReplacer("__BETA__"))
@@ -19,7 +17,6 @@ directRun = ExternalRun("DIRECT","SU2_CFD settings_tmpl.cfg")
 directRun.addConfig("settings_tmpl.cfg")
 directRun.addConfig("element_properties.dat")
 directRun.addData("mesh.su2")
-directRun.addParameter(pType_direct)
 directRun.addParameter(fType_objective)
 directRun.addParameter(beta)
 
@@ -28,7 +25,6 @@ adjointRun1.addConfig("settings_tmpl.cfg")
 adjointRun1.addConfig("element_properties.dat")
 adjointRun1.addData("mesh.su2")
 adjointRun1.addData("DIRECT/solution.dat")
-adjointRun1.addParameter(pType_adjoint)
 adjointRun1.addParameter(fType_objective)
 adjointRun1.addParameter(beta)
 
@@ -37,20 +33,19 @@ adjointRun2.addConfig("settings_tmpl.cfg")
 adjointRun2.addConfig("element_properties.dat")
 adjointRun2.addData("mesh.su2")
 adjointRun2.addData("DIRECT/solution.dat")
-adjointRun2.addParameter(pType_adjoint)
 adjointRun2.addParameter(fType_constraint)
 adjointRun2.addParameter(beta)
 
 # Functions
-fun1 = Function("reference_node","DIRECT/history.csv",TableReader(0,0,(1,0)))
+fun1 = Function("reference_node","DIRECT/history.csv",LabeledTableReader('"RefNode"'))
 fun1.addInputVariable(rho,"ADJOINT1/grad.dat",TableReader(None,0))
 fun1.addValueEvalStep(directRun)
 fun1.addGradientEvalStep(adjointRun1)
 
-fun2 = Function("solid_fraction","ADJOINT2/history.csv",TableReader(0,0,(1,0)))
+fun2 = Function("solid_fraction","DIRECT/history.csv",LabeledTableReader('"VolFrac"'))
 fun2.addInputVariable(rho,"ADJOINT2/grad.dat",TableReader(None,0))
 fun2.addValueEvalStep(directRun)
-fun2.addValueEvalStep(adjointRun2)
+fun2.addGradientEvalStep(adjointRun2)
 
 # Driver
 update_iters = 25
